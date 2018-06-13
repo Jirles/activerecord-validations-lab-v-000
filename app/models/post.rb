ef3ade9ccf4976ade8d488353e6d0e9757107bee
Emily Jirles <emily.jirles@gmail.com>
2018-06-13 12:00:53 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   validates :content, length { minimum: 250 }
   validates :summary, length { maximum: 250 }
   validates :category, inclusion { in: %w(Fiction, Non-Fiction)}
-  validates_each :title do 
+  validates_each :title do |
 end
 
